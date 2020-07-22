588d3079f79f8aaabeefe3e7e8484871f6bb1767
Tom-Lucas Säger <tlsaeger@icloud.com>
2020-07-22 17:24:19 +0200

    added image gallery

--- app.js
+++ app.js
@@ -2,49 +2,57 @@ var locations = [{
     lat: 53.550415,
     lng: 10.109466,
     labels: "03.06.20",
-    streetName: "Schiffbeker Weg/Steinadlerweg"
+    streetName: "Schiffbeker Weg/Steinadlerweg",
+    year: 2020
   },
   {
     lat: 53.599232,
     lng: 9.993986,
     labels: "07.04.20",
-    streetName: "Meenkwiese/Bebelallee"
+    streetName: "Meenkwiese/Bebelallee",
+    year: 2020
   },
   {
     lat: 53.574697,
     lng: 10.074822,
     labels: "13.01.20",
-    streetName: "Rüterstraße/Wendemuthstraße"
+    streetName: "Rüterstraße/Wendemuthstraße",
+    year: 2020
   },
   {
     lat: 53.560935,
     lng: 10.013736,
     labels: "15.07.19",
-    streetName: "An der Alster / Lohmühlenstraße"
+    streetName: "An der Alster / Lohmühlenstraße",
+    year: 2019
   },
   {
     lat: 53.572961,
     lng: 9.936176,
     labels: "19.03.19",
-    streetName: "Holstenkamp/Großen Bahnstraße"
+    streetName: "Holstenkamp/Großen Bahnstraße",
+    year: 2019
   },
   {
     lat: 53.573906,
     lng: 9.961272,
     labels: "07.05.18",
-    streetName: "Osterstraße Ecke Eppendorfer Weg"
+    streetName: "Osterstraße Ecke Eppendorfer Weg",
+    year: 2018
   },
   {
     lat: 53.567306,
     lng: 10.044876,
     labels: "2016",
-    streetName: "Ecke Ritter­straße/Wandsbeker Chaussee "
+    streetName: "Ecke Ritter­straße/Wandsbeker Chaussee ",
+    year: 2016
   },
   {
     lat: 53.565047,
     lng: 10.019017,
     labels: "30.01.2014",
-    streetName: "Mundsburger Damm/Arm­gartstraße"
+    streetName: "Mundsburger Damm/Arm­gartstraße",
+    year: 2014
   }
 ]
 var images = [
@@ -53,6 +61,21 @@ var images = [
   "2",
   "3",
   "4",
+  /* "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19", */
 ]
 var map;
 var newImg;
@@ -300,22 +323,21 @@ function initMap() {
 }
 
 
-function detailPictures() {
-  for (i = 0; i < locations.length; i++) {
-    var classNumber = i % 2;
+function galleryPictures() {
+  for (i = 0; i < images.length; i++) {
+    /* var classNumber = i % 2; */
 
     var newContainer = document.createElement("div");
-    newContainer.classList.add('detailContainer', 'detailContainer' + classNumber);
-    newContainer.setAttribute("data-aos", "fade-in")
-    var section = document.getElementById("detailPicture");
-    section.appendChild(newContainer);
+    newContainer.classList.add('galleryContainerImg');
+    var section = document.getElementById("galleryImgFlexContainer");
+    section.appendChild(newContainer); 
 
     var newImg = document.createElement("img");
-    newImg.src = "Assets/Bild_3.png";
-    newImg.setAttribute('class', 'snippet');
+    newImg.src = "Assets/img/ghost_bike_gallery" + images[i] + ".jpg";
+    newImg.setAttribute('class', 'galleryImg');
     newContainer.appendChild(newImg);
 
-    var descriptionText = document.createElement("h2");
+ /*    var descriptionText = document.createElement("h2");
     descriptionText.classList.add("descritptionTextHeadline")
     descriptionText.innerHTML = locations[i].labels;
     newContainer.appendChild(descriptionText);
@@ -323,9 +345,19 @@ function detailPictures() {
     var streetName = document.createElement("p");
     streetName.classList.add("descritptionTextBody");
     streetName.innerHTML = locations[i].streetName;
-    newContainer.appendChild(streetName);
+    newContainer.appendChild(streetName); */
 
 
   }
 }
-/* detailPictures(); */
+galleryPictures(); 
+
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+} 
